d6b69bb7cb39959b6016c51e83748b704f4d509d
Sasha <shashwatipchakraborty@gmail.com>
2026-02-28 23:26:31 +0530

    Add files

---
+++ Solution1.py
@@ -0,0 +1,15 @@
+from diagrams import Diagram,Edge
+from diagrams.aws.general import Users
+from diagrams.aws.network import Route53
+from diagrams.aws.storage import SimpleStorageServiceS3 as S3
+from diagrams.aws.network import CloudFront as CF
+from diagrams.aws.security import IdentityAndAccessManagementIamRole as IAMRole
+
+
+with Diagram("Solution1") as diag:
+    Users("User") >> Edge(color="firebrick",style="dashed") >> \
+    Route53("Route53") >> Edge(color="firebrick",style="dashed") >> \
+    CF("Cloud Front")  >> Edge(color="firebrick",style="dashed") >> \
+    S3("Simple Storage Service")  >> Edge(color="firebrick",style="dashed") 
+
+
